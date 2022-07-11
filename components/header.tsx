21695679123999f9226554766a8445f8b22782fe
Wojciech Fiszer <fiszer.wojciech@gmail.com>
2022-07-12 01:32:58 +0200

    Added hamburger menu for small screens

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 
 type Props = {}
@@ -14,22 +14,49 @@ const links: {
 ]
 
 export default function Header({}: Props): JSX.Element {
+  const [open, setOpen] = useState(false)
+
   return (
-    <header className="container mx-auto md:px-8 px-4 max-w-[1000px] flex justify-between">
-      <h1 className="text-2xl font-semibold py-3">Wojciech Fiszer</h1>
-      <nav className="flex items-center">
-        <ul className="flex h-full items-center">
-          {links.map(({ text, href }) => (
-            <li key={text} className="flex h-full items-center">
-              <Link href={href}>
-                <a className="flex items-center py-2 px-4 rounded-full hover:bg-gray-100 hover:text-gray-800">
-                  {text}
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+    <header className="container mx-auto md:px-8 px-4 max-w-[1000px]">
+      <div className="flex justify-between">
+        <button
+          className="flex md:hidden flex-col justify-center"
+          onClick={() => setOpen((prevState) => !prevState)}
+        >
+          <div className="bg-black h-1 w-6" />
+          <div className="bg-black h-1 w-6 mt-1" />
+          <div className="bg-black h-1 w-6 mt-1" />
+        </button>
+        <h1 className="text-2xl font-semibold py-3">Wojciech Fiszer</h1>
+        <nav className="hidden sm:flex items-center">
+          <ul className="flex">
+            {links.map(({ text, href }) => (
+              <li key={text}>
+                <Link href={href}>
+                  <a className="flex py-2 px-4 rounded-full hover:bg-gray-100 hover:text-gray-800">
+                    {text}
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+      {open && (
+        <nav className="md:hidden">
+          <ul>
+            {links.map(({ text, href }) => (
+              <li key={text}>
+                <Link href={href}>
+                  <a className="flex w-full py-2 px-4 rounded-full hover:bg-gray-100 hover:text-gray-800">
+                    {text}
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
     </header>
   )
 }
