f1208f3c1f3da5e70736314d18d2a605d22db141
Wojciech Fiszer <fiszer.wojciech@gmail.com>
2022-07-12 13:53:31 +0200

    Changed work experience timeline

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import Image from 'next/image'
 
 type TimelineItemData = {
+  companyLogo: string
+  companyName: string
   time: string
   title: string
   text: string
@@ -13,31 +16,34 @@ type Props = {
   data: TimelineData
 }
 
-function Dot(): JSX.Element {
-  return (
-    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-  )
-}
-
 export default function Timeline({ className, data }: Props): JSX.Element {
   return (
     <ol
       className={`${
         className ? `${className} ` : ''
-      }relative border-l border-gray-200 dark:border-gray-700`}
+      }relative border-l border-gray-400`}
     >
-      {data.map(({ time, title, text }) => (
-        <li key={title} className="ml-4">
-          <Dot />
-          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-            {time}
-          </time>
-          <h4 className="text-lg font-semibold text-gray-900 dark:text-white">
-            {title}
-          </h4>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-            {text}
-          </p>
+      {data.map(({ companyLogo, companyName, time, title, text }) => (
+        <li
+          key={`${companyName}-${title}`}
+          className="ml-4 border rounded-2xl p-2 m-2"
+        >
+          <div className="absolute w-3 h-3 bg-gray-400 rounded-full mt-1.5 -left-1.5 border border-white"></div>
+          <div className="flex gap-6">
+            <Image
+              width="150"
+              height="40"
+              src={companyLogo}
+              alt={companyName}
+            />
+            <div>
+              <time className="mb-1 text-sm font-normal text-gray-400">
+                {time}
+              </time>
+              <h4 className="text-lg font-semibold text-gray-900">{title}</h4>
+            </div>
+          </div>
+          <p className="text-gray-500">{text}</p>
         </li>
       ))}
     </ol>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,75 +1,51 @@
 import type { NextPage } from 'next'
 import Layout from '../components/layout'
 import Timeline, { TimelineData } from '../components/timeline'
-import Image from 'next/image'
 import useTypedText from '../hooks/useTypedText'
 import TextCursor from '../components/text-cursor'
 
-type WorkExperience = {
-  companyName: string
-  companyLogo: string
-  timelineData: TimelineData
-}[]
-
-const workExperience: WorkExperience = [
+const workExperience: TimelineData = [
   {
     companyName: 'Tink',
     companyLogo: '/tink-logo.svg',
-    timelineData: [
-      {
-        time: 'June 2022',
-        title: 'Senior Software Engineer',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi sit amet mauris commodo quis imperdiet massa tincidunt nunc. Sit amet nisl purus in mollis nunc sed id semper. Posuere ac ut consequat semper viverra nam libero. Dignissim convallis aenean et tortor at risus viverra.',
-      },
-      {
-        time: 'May 2021',
-        title: 'Software Engineer',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh nisl condimentum id venenatis a condimentum vitae sapien. Eleifend donec pretium vulputate sapien nec. Nam libero justo laoreet sit amet. Ornare arcu dui vivamus arcu felis bibendum..',
-      },
-    ],
+    time: 'June 2022',
+    title: 'Senior Software Engineer',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi sit amet mauris commodo quis imperdiet massa tincidunt nunc. Sit amet nisl purus in mollis nunc sed id semper. Posuere ac ut consequat semper viverra nam libero. Dignissim convallis aenean et tortor at risus viverra.',
+  },
+  {
+    companyName: 'Tink',
+    companyLogo: '/tink-logo.svg',
+    time: 'May 2021',
+    title: 'Software Engineer',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh nisl condimentum id venenatis a condimentum vitae sapien. Eleifend donec pretium vulputate sapien nec. Nam libero justo laoreet sit amet. Ornare arcu dui vivamus arcu felis bibendum..',
   },
   {
     companyName: 'Danske Bank',
     companyLogo: '/danske-bank-logo.svg',
-    timelineData: [
-      {
-        time: 'August 2020',
-        title: 'Senior Java Developer',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sagittis nisl rhoncus mattis rhoncus urna neque viverra justo nec. Quis risus sed vulputate odio ut. Pharetra convallis posuere morbi leo urna. Neque sodales ut etiam sit amet nisl purus.',
-      },
-    ],
+    time: 'August 2020',
+    title: 'Senior Java Developer',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sagittis nisl rhoncus mattis rhoncus urna neque viverra justo nec. Quis risus sed vulputate odio ut. Pharetra convallis posuere morbi leo urna. Neque sodales ut etiam sit amet nisl purus.',
   },
   {
     companyName: 'Bosch',
     companyLogo: '/bosch-logo.svg',
-    timelineData: [
-      {
-        time: 'June 2019',
-        title: 'Software Developer',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet purus gravida quis blandit turpis cursus. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. In hendrerit gravida rutrum quisque non tellus. Quam elementum pulvinar etiam non.',
-      },
-    ],
+    time: 'June 2019',
+    title: 'Software Developer',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet purus gravida quis blandit turpis cursus. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. In hendrerit gravida rutrum quisque non tellus. Quam elementum pulvinar etiam non.',
   },
   {
     companyName: 'StepStone',
     companyLogo: '/stepstone-logo.svg',
-    timelineData: [
-      {
-        time: 'February 2018',
-        title: 'Big Data Developer',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet nisl purus in mollis nunc sed id. Curabitur gravida arcu ac tortor. Nec dui nunc mattis enim ut tellus elementum sagittis. Eu tincidunt tortor aliquam nulla facilisi cras.',
-      },
-      {
-        time: 'June 2016',
-        title: 'Junior Java Developer/Java Developer',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet nisl purus in mollis nunc sed id. Curabitur gravida arcu ac tortor. Nec dui nunc mattis enim ut tellus elementum sagittis. Eu tincidunt tortor aliquam nulla facilisi cras.',
-      },
-      {
-        time: 'February 2016',
-        title: 'Software Engineer Intern',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nec dui nunc mattis enim ut. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper. Ut porttitor leo a diam sollicitudin tempor id eu nisl. Et malesuada fames ac turpis egestas.',
-      },
-    ],
+    time: 'February 2018',
+    title: 'Big Data Developer',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet nisl purus in mollis nunc sed id. Curabitur gravida arcu ac tortor. Nec dui nunc mattis enim ut tellus elementum sagittis. Eu tincidunt tortor aliquam nulla facilisi cras.',
+  },
+  {
+    companyName: 'StepStone',
+    companyLogo: '/stepstone-logo.svg',
+    time: 'February 2016',
+    title: 'Java Developer/Junior Java Developer/Software Engineer Intern',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet nisl purus in mollis nunc sed id. Curabitur gravida arcu ac tortor. Nec dui nunc mattis enim ut tellus elementum sagittis. Eu tincidunt tortor aliquam nulla facilisi cras.',
   },
 ]
 
@@ -99,17 +75,7 @@ const Home: NextPage = () => {
         <h2 id="work-experience" className="pt-5 text-2xl">
           Work experience
         </h2>
-        {workExperience.map(({ companyName, companyLogo, timelineData }) => (
-          <div key={companyName} className="pt-5">
-            <Image
-              width="150"
-              height="40"
-              src={companyLogo}
-              alt={companyName}
-            />
-            <Timeline data={timelineData} />
-          </div>
-        ))}
+        <Timeline data={workExperience} />
       </main>
     </Layout>
   )
