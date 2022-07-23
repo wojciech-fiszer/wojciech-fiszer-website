056c1529328d3bb6baf41a47c3ca3d315d5ecd2d
Wojciech Fiszer <fiszer.wojciech@gmail.com>
2022-07-23 16:56:43 +0200

    Align header with new design

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Link from 'next/link'
 
 type Props = {}
@@ -7,33 +7,28 @@ const links: {
   text: string
   href: string
 }[] = [
+  { text: 'Home', href: '#home' },
   { text: 'About', href: '#about' },
-  { text: 'Stack', href: '#stack' },
-  { text: 'Experience', href: '#work-experience' },
+  { text: 'Experience', href: '#experience' },
+  { text: 'Skills', href: '#skills' },
   { text: 'Contact', href: '#contact' },
 ]
 
 export default function Header({}: Props): JSX.Element {
-  const [open, setOpen] = useState(false)
-
   return (
-    <header className="container mx-auto md:px-8 px-4 max-w-[1000px]">
-      <div className="flex justify-between">
-        <button
-          className="flex md:hidden flex-col justify-center"
-          onClick={() => setOpen((prevState) => !prevState)}
-        >
-          <div className="bg-black h-1 w-6" />
-          <div className="bg-black h-1 w-6 mt-1" />
-          <div className="bg-black h-1 w-6 mt-1" />
-        </button>
-        <h1 className="text-2xl font-semibold py-3">Wojciech Fiszer</h1>
+    <header className="bg-neutral-900 h-32 text-white">
+      <div className="container mx-auto flex justify-between items-center h-full">
+        <h1 className="text-2xl font-mono font-semibold">
+          <span className="text-orange-500">&lt;</span>
+          Wojciech Fiszer
+          <span className="text-orange-500">/&gt;</span>
+        </h1>
         <nav className="hidden sm:flex items-center">
-          <ul className="flex">
+          <ul className="flex items-center gap-20">
             {links.map(({ text, href }) => (
               <li key={text}>
                 <Link href={href}>
-                  <a className="flex py-2 px-4 rounded-full hover:bg-gray-100 hover:text-gray-800">
+                  <a className="flex uppercase font-semibold transition duration-150 border-b-4 border-transparent hover:border-orange-500">
                     {text}
                   </a>
                 </Link>
@@ -42,21 +37,6 @@ export default function Header({}: Props): JSX.Element {
           </ul>
         </nav>
       </div>
-      {open && (
-        <nav className="md:hidden">
-          <ul>
-            {links.map(({ text, href }) => (
-              <li key={text}>
-                <Link href={href}>
-                  <a className="flex w-full py-2 px-4 rounded-full hover:bg-gray-100 hover:text-gray-800">
-                    {text}
-                  </a>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      )}
     </header>
   )
 }
